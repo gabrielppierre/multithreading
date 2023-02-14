6b34a34b383ca24bb9b363884c0417565096b7bd
Gabriel Pierre <94745573+gabrielppierre@users.noreply.github.com>
2023-02-13 23:09:30 -0300

    desafio 2

---
+++ desafio2.c
@@ -0,0 +1,18 @@
+/* Crie um programa com 8 threads usando laço.
+Metade das threads fazem 10 depósitos de 20 reais a cada 1 segundo.
+A outra metade das threads fazem retiradas de 20 reais a cada 1
+segundo.
+O saldo inicia zerado.
+O saldo nunca pode ficar negativo. Se uma retirada for solicitada e o
+saldo é menor do que 20 reais, apenas o valor disponível é retirado.
+Após as 8 threads terminarem a thread principal (main) coloca na tela
+o saldo final. */
+
+#include <stdio.h>
+#include <pthread.h>
+
+int main(){
+
+
+    return 0;
+}
